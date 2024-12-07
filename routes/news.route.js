117e41deb0daf85acba3eeb22cd87fe6f60ff182
Tran Trung Tin <trantrungtin254@gmail.com>
2024-12-07 10:38:48 +0700

    catnews, latestnews in news-category

--- routes/news.route.js
+++ routes/news.route.js
@@ -2,8 +2,14 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/details', (req, res) => {
-    res.render('vwNews/news-detail',{
-        
+    res.render('vwNews/news-detail', {
+
+    });
+});
+
+router.get('/category', (req, res) => {
+    res.render('vwNews/news-category', {
+
     });
 });
 
