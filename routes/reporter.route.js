168a120879cbd96f127b1a67fc85424475c5d9a7
MrHH-k22 <nguyenmaihuyhoang156@gmail.com>
2024-12-11 17:34:52 +0700

    update handlerbars to bootstrap 5

--- routes/reporter.route.js
+++ routes/reporter.route.js
@@ -3,51 +3,55 @@ const router = express.Router();
 
 
 
-router.use(function(req, res, next) {
-    res.locals.Buttons =[
-        { label: 'Dashboard', url: '/reporter/dashboard', icon: 'bi bi-file-earmark' },
-        { label: 'Post', url: '/reporter/post', icon: 'bi bi-archive', id:'2' },
-
+router.use(function (req, res, next) {
+    res.locals.items = [
+        { label: 'Dashboard', url: '/reporter/dashboard', icon: 'bi bi-file-earmark', isDropdown: false },
+        {
+            label: 'Post',
+            icon: 'bi bi-archive',
+            isDropdown: true,
+            options: [
+                { label: 'Add News', url: '/reporter/news', icon: 'bi bi-journal-text' },
+                { label: 'Category&Tag', url: '/reporter/category&tag', icon: 'bi bi-person-check' }
+            ]
+        }
     ]
-    res.locals.DropdownButtons = [
-        { label: 'Add News', url: '/reporter/news', icon: 'bi bi-journal-text', id: '2' },
-        { label: 'Category&Tag', url: '/reporter/category&tag', icon: 'bi bi-person-check', id: '2' },
-    ];
     next();
-})
+});
 
 router.get('*', (req, res) => {
     const currentPath = req.originalUrl;
-    res.render('vwReporter/mainreporter', 
-        { 
-            layout: false,
-            currentPath 
+    res.render('vwReporter/mainreporter',
+        {
+            layout: 'user',
+            currentPath
         });
 });
 
 
-router.get('/', function(req, res){
+router.get('/', function (req, res) {
     res.render('vwReporter/mainreporter', {
-        layout: false,
-       
+        layout: 'user',
+        items: res.locals.items,
+
     });
 })
-router.get('/dashboard',function(req, res){
-    res.render('vwReporter/mainreporter',{
-        layout: false,
-        
-    })
-})
-router.get('/category&tag',function(req,res){
+router.get('/dashboard', function (req, res) {
+    res.render('vwReporter/mainreporter', {
+        layout: 'user',
 
-    res.render('vwReporter/mainreporter',{
-        layout: false,
     })
-    
 })
-router.get('/news',function(req,res){
-    res.render('vwReporter/mainreporter',{
-        layout: false,
+router.get('/category&tag', function (req, res) {
+
+    res.render('vwReporter/mainreporter', {
+        layout: 'user',
+    })
+
+})
+router.get('/news', function (req, res) {
+    res.render('vwReporter/mainreporter', {
+        layout: 'user',
     })
 })
 export default router;
