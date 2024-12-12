c43b2ba7244d7e6c211a0c35ccac84bdf4be19e0
Nhat an <ngnhatan1205@gmail.com>
2024-12-12 14:43:06 +0700

    change new sidebar

--- routes/reporter.route.js
+++ routes/reporter.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.use(function (req, res, next) {
     res.locals.items = [
-        { label: 'Dashboard', url: '/reporter/dashboard', icon: 'bi bi-file-earmark', isDropdown: false },
+        { label: 'Dashboard', url: '/reporter', icon: 'bi bi-file-earmark', isDropdown: false },
         {
             label: 'Post',
             icon: 'bi bi-archive',
@@ -20,24 +20,10 @@ router.use(function (req, res, next) {
     next();
 });
 
-router.get('*', (req, res) => {
-    const currentPath = req.originalUrl;
-    res.render('vwReporter/mainreporter',
-        {
-            layout: 'user',
-            currentPath
-        });
-});
+
 
 
 router.get('/', function (req, res) {
-    res.render('vwReporter/mainreporter', {
-        layout: 'user',
-        items: res.locals.items,
-
-    });
-})
-router.get('/dashboard', function (req, res) {
     res.render('vwReporter/mainreporter', {
         layout: 'user',
 
@@ -45,50 +31,48 @@ router.get('/dashboard', function (req, res) {
 })
 router.get('/category&tag', function (req, res) {
 
-    res.render('vwReporter/mainreporter', {
+    res.render('vwReporter/listnews', {
         layout: 'user',
     })
 
 })
 
 router.get('/news', async function(req,res){
-    try {
-        const list = await categoryService.findall();
-        console.log(list);
-        res.render('vwReporter/mainreporter',{
-            layout: 'user',
-            categories: list
-        });
-    } catch(err) {
-        console.error(err);
-        res.status(500).send('Database error');
-    }
+    
+    const list = await categoryService.findall();
+    // console.log(list);
+    res.render('vwReporter/postnews',{
+        layout: 'user',
+        categories: list
+    });
+    
 });
 
-router.post('/news',  function(req, res){
 
-    const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-          cb(null, './static/imgs')
-          // có thể tùy chỉnh thư mục để lưu linh động
-        },
-        filename: function (req, file, cb) {
-          cb(null, file.originalname)
-        }
-      })
-    const upload =multer({storage})
-    upload.array('fulMain',5)(req, res,function(err){
-        console.log(req.body);
-        if(err) {
-            console.error(err)
-        }
-        else{
-            res.render('vwMisc/upload');
+// router.post('/news',  function(req, res){
 
-        }
+//     const storage = multer.diskStorage({
+//         destination: function (req, file, cb) {
+//           cb(null, './static/imgs')
+//           // có thể tùy chỉnh thư mục để lưu linh động
+//         },
+//         filename: function (req, file, cb) {
+//           cb(null, file.originalname)
+//         }
+//       })
+//     const upload =multer({storage})
+//     upload.array('fulMain',5)(req, res,function(err){
+//         console.log(req.body);
+//         if(err) {
+//             console.error(err)
+//         }
+//         else{
+//             res.render('vwMisc/upload');
 
-    })
-    //console.log(req.body);
+//         }
+
+//     })
+//     //console.log(req.body);
     
-})
+// })
 export default router;
