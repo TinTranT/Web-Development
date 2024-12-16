2e1013eb28f0cfdeb37724970835cc6465cbc7b3
Tran Trung Tin <trantrungtin254@gmail.com>
2024-12-16 15:28:54 +0700

    complete UpdateProfile, add back button on pages related to profile

--- services/account.service.js
+++ services/account.service.js
@@ -9,14 +9,22 @@ export default {
         return db('account').insert(entity);
     },
 
+    update(entity) {
+        return db('account')
+            .where('Id', entity.Id)
+            .update(entity);
+    },
+
     updatePassword(entity) {
         return db('account')
             .where('Id', entity.Id)
             .update({ Password: entity.Password });
     },
-    findPageByRole(role,limit,offset) {
+
+    findPageByRole(role, limit, offset) {
         return db('account').where('Role', role).limit(limit).offset(offset);
     },
+
     countbyRole(role) {
         return db('account').where('Role', role).count('* as total');
     }
