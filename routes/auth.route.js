4f27281ef07d9e9ecaab21396ceeda02cf74a5ac
Tran Trung Tin <trantrungtin254@gmail.com>
2024-12-19 20:44:53 +0700

    Log in with Github

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { Strategy as GitHubStrategy } from 'passport-github2';
 import bcrypt from 'bcryptjs';
 import accountService from '../services/account.service.js';
+import dotenv from 'dotenv';
 
-const GOOGLE_CLIENT_ID = '';
-const GOOGLE_CLIENT_SECRET = '';
+dotenv.config();
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 const router = express.Router();
 
@@ -16,6 +23,16 @@ passport.use(new GoogleStrategy({
 },
     function (accessToken, refreshToken, profile, cb) {
         return cb(null, profile);
+    }
+));
+
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:5555/auth/github/callback"
+},
+    function (accessToken, refreshToken, profile, done) {
+        return done(null, profile);
     }
 ));
 
@@ -90,5 +107,64 @@ router.get('/google/success', async (req, res) => {
         }
     }
 });
+
+router.get('/github',
+    passport.authenticate('github', { scope: ['user:email'] }));
+
+router.get('/github/callback',
+    passport.authenticate('github', { failureRedirect: '/?err_message=Something went wrong with Github login' }),
+    async function (req, res) {
+        // Successful authentication
+        // res.redirect('/');
+
+        console.log(req.user);
+
+        const email = req.user.username + "@github.com";
+        const user = await accountService.findByEmail(email);
+        if (!user) {
+            const hash_password = bcrypt.hashSync(email, 8);
+            const entity = {
+                Email: email,
+                Password: hash_password,
+                Name: req.user.username,
+                PenName: req.user.username,
+                Role: 1
+            }
+            const ret = await accountService.add(entity);
+
+            // Save session
+            const user = await accountService.findByEmail(email);
+            req.session.authUser = user;
+            req.session.auth = true;
+
+            // Load previous path
+            let retUrl = req.session.retUrl || '/';
+            retUrl = retUrl + "?success_message=Your password is your name with @github.com";
+            req.session.retUrl = null;
+
+            //Access previous path
+            res.redirect(retUrl);
+        } else {
+            // Save session
+            req.session.authUser = user;
+            req.session.auth = true;
+
+            if (req.session.retUrl) {
+                let retUrl = req.session.retUrl;
+                req.session.retUrl = null;
+                res.redirect(retUrl);
+            } else {
+                if (user.Role === 4) {
+                    res.redirect('/admin');
+                } else if (user.Role === 3) {
+                    res.redirect('/editor');
+                } else if (user.Role === 2) {
+                    res.redirect('/reporter');
+                } else {
+                    res.redirect('/');
+                }
+            }
+        }
+    });
 
 export default router;
