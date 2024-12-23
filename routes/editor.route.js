c2c500ef2d97852e7b9a2bc62b59f45037456fb1
Nhat an <ngnhatan1205@gmail.com>
2024-12-23 12:24:03 +0700

    complete published list for editor

--- routes/editor.route.js
+++ routes/editor.route.js
@@ -14,6 +14,7 @@ router.use(function (req, res, next) {
         { label: 'Draft Articles', url: '/editor/draft-articles', icon: 'bi bi-archive', isDropdown: false },
         { label: 'Pending Publication', url: '/editor/pending-publication', icon: 'bi bi-hourglass-split', isDropdown: false },
         { label: 'Rejected Articles', url: '/editor/rejected', icon: 'bi bi-x-circle-fill', isDropdown: false },
+        { label: 'Published Articles', url: '/editor/published', icon: 'bi bi-cloud-upload-fill', isDropdown: false },
     ]
     next();
 });
@@ -200,8 +201,6 @@ router.get('/rejected', PreviousUrl, async function (req, res) {
         nRows = await rejectreasonService.findCountRejectedArticlesByCat(id, accountID); // Tổng số bài theo category
         news = await newsService.findPageForRejectedArticlesByCat(id, limit, offset, accountID); // Lấy bài theo category
     }
-    console.log(nRows);
-    console.log(news);
     const nPages = Math.ceil(nRows.total / limit);
     const page_items = [];
     for (let i = 1; i <= nPages; i++) {
@@ -223,5 +222,43 @@ router.get('/rejected', PreviousUrl, async function (req, res) {
         previousPage: page > 1 ? page - 1 : 1,
         nextPage: page < nPages ? page + 1 : nPages,
     })
+});
+router.get('/published', PreviousUrl, async function (req, res) {
+    const accountID = req.session.authUser.Id;
+    const catlist = await categoryService.findWithParentAndEditor(accountID);
+    const id = parseInt(req.query.id) || 0;
+    const limit = 4;
+    const page = parseInt(req.query.page) || 1;
+    const offset = (page - 1) * limit;
+    let nRows, news;
+    if (id === 0) {
+        nRows = await newsService.findCountAllArticles(accountID, 3); // Tổng số bài viết
+        news = await newsService.findPageForAllArticles(accountID, limit, offset, 3); // Lấy tất cả bài viết
+    } else {
+        nRows = await newsService.findCountArticlesByCat(id, accountID, 3); // Tổng số bài theo category
+        news = await newsService.findPageForArticlesByCat(id, limit, offset, accountID, 3); // Lấy bài theo category
+    }
+    const nPages = Math.ceil(nRows.total / limit);
+    const page_items = [];
+    for (let i = 1; i <= nPages; i++) {
+        const item = {
+            value: i,
+            isActive: i === page,
+        }
+        page_items.push(item);
+    }
+    // console.log(cat);
+    res.render('vwEditor/published-list', {
+        layout: 'user',
+        news: news,
+        catList: catlist,
+        empty: news.length === 0,
+        page_items: page_items,
+        catId: id,
+        isFirstPage: page === 1,
+        isLastPage: page === nPages,
+        previousPage: page > 1 ? page - 1 : 1,
+        nextPage: page < nPages ? page + 1 : nPages,
+    });
 });
 export default router;
